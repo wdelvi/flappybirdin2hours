254917ef447170319a52bf43a09648428bf00365
Will Delventhal <will.delventhal@gmail.com>
2022-10-04 07:31:00 -0700

    Functional Flappy Bird

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour 
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour 
+{
+    public float jumpAmount = 5f;
+
+    public void Jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, jumpAmount);
+    }
+}
