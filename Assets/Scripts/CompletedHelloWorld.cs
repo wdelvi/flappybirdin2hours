bbedf52ab90aa8ec1f034e1cd38a55bee7c4cbac
Celtican <Celtican5@gmail.com>
2022-10-12 19:50:53 -0400

    Renamed scripts to match new version of course

--- Assets/Scripts/CompletedHelloWorld.cs
+++ Assets/Scripts/CompletedHelloWorld.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class CompletedHelloWorld : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CompletedScoreKeeper : MonoBehaviour
+public class ScoreKeeper : MonoBehaviour
 {
     public Text scoreText;
 
